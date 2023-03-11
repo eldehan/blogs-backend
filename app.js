168032d956f8da39402b97fe83fed3c90319906f
Hans Elde <hanserik.elde@gmail.com>
2023-03-10 20:36:35 -0800

    deleted unnecessary comments in app.js

--- app.js
+++ app.js
@@ -4,25 +4,5 @@ import appLoader from './loaders/index.js'
 export async function createServer() {
   const app = express();
   await appLoader({ app })
-  // (await import('./loaders/index.js')).default({ app })
   return app
 }
-
-
-
-/*
-export const start = async () => {
-  try {
-    await appLoader({ app, express });
-    app.listen(appConfig.port, () => {
-       
-      console.log(`REST API on http://localhost:${appConfig.port}/api/v1`);
-      console.log(process.env.MONGO_URI_DEV);
-      console.log(appConfig.dbUrl);
-      
-    });
-  } catch (e) {
-    console.error(e)
-  }
-}
-*/
